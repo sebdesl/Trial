4ed72bb3bfae49efdad72dde99f637a6eac3032b
seDesla <seb.lacrosse@gmail.com>
2017-01-30 19:30:19 -0500

    adding a c file

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FirstProjectC
+//
+//  Created by Sebastien Deslandres on 2017-01-30.
+//  Copyright © 2017 Sebastien Deslandres. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
